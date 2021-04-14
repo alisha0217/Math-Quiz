4ff9c4445224bfc697c9ecdfab8e5250f949a13e
alisha0217 <69092959+alisha0217@users.noreply.github.com>
2021-04-13 21:19:45 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -16,9 +16,9 @@ document.getElementById("player_answer").innerHTML = "Answer turn - " + player2_
 function send(){
     number1 = document.getElementById("number1").value;
     number2 = document.getElementById("number2").value;
-    actual_answer = parsInt(number1) * parsInt(number2);
-
-    question_word = "<h4 id = 'problem_display' > Q. " + number1 + "X" + number2 + "</h4>";
+    actual_answer = parseInt(number1) * parseInt(number2);
+ 
+    question_word = "<h4 id = 'problem_display' > Q. " + number1 + " X " + number2 + "</h4>";
     input_box = "<br> answer: <input type = 'text' id = 'input_check_box' >";
     check_button = "<br><br> <button class = 'btn btn-info' onclick = 'check();' > Check </button>";
     row = question_word + input_box + check_button;
